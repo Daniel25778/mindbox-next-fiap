6367ca4fe3dbb1fdf76f1cf30b62e72c1f088d70
GabrielTSR <gabrielalvesrocha22@gmail.com>
2024-06-01 18:56:29 -0300

    feat: icons for default categories

--- app/src/main/java/br/com/mindbox/database/mock/DefaultEmailCategories.kt
+++ app/src/main/java/br/com/mindbox/database/mock/DefaultEmailCategories.kt
@@ -1,6 +1,6 @@
 package br.com.mindbox.database.mock
 
-import androidx.core.R
+import br.com.mindbox.R
 import br.com.mindbox.model.email.EmailCategory
 import br.com.mindbox.model.email.EmailCategoryName
 
@@ -10,48 +10,48 @@ class DefaultEmailCategories {
             return listOf(
                 EmailCategory(
                     name = EmailCategoryName.EDUCATION,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_education_icon,
+                    iconResIdWhite = R.drawable.email_category_white_education_icon
                 ),
                 EmailCategory(
                     name = EmailCategoryName.WORK,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_work_icon,
+                    iconResIdWhite = R.drawable.email_category_white_work_icon
                 ),
                 EmailCategory(
                     name = EmailCategoryName.PERSONAL,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_person_icon,
+                    iconResIdWhite = R.drawable.email_category_white_person_icon
                 ),
                 EmailCategory(
                     name = EmailCategoryName.PROMOTION,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_offer_icon,
+                    iconResIdWhite = R.drawable.email_category_white_offer_icon
                 ),
                 EmailCategory(
                     name = EmailCategoryName.FINANCE,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_coin_icon,
+                    iconResIdWhite = R.drawable.email_category_white_coin_icon
                 ),
                 EmailCategory(
                     name = EmailCategoryName.HEALTH,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_heart_icon,
+                    iconResIdWhite = R.drawable.email_category_white_heart_icon
                 ),
                 EmailCategory(
                     name = EmailCategoryName.TRAVEL,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_plane_icon,
+                    iconResIdWhite = R.drawable.email_category_white_plane_icon
                 ),
                 EmailCategory(
                     name = EmailCategoryName.NEWSLETTER,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_journal_icon,
+                    iconResIdWhite = R.drawable.email_category_white_journal_icon
                 ),
                 EmailCategory(
                     name = EmailCategoryName.OTHERS,
-                    iconResIdPurple = R.drawable.ic_call_decline,
-                    iconResIdWhite = R.drawable.ic_call_decline
+                    iconResIdPurple = R.drawable.email_category_purple_folder_icon,
+                    iconResIdWhite = R.drawable.email_category_white_folder_icon
                 )
             )
         }
