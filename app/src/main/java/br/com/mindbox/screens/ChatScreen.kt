83e2f2cb8a64f1f854c7d81c3138faf1322f1876
GabrielTSR <gabrielalvesrocha22@gmail.com>
2024-06-02 14:11:08 -0300

    refactor: some warnings removed

--- app/src/main/java/br/com/mindbox/screens/ChatScreen.kt
+++ app/src/main/java/br/com/mindbox/screens/ChatScreen.kt
@@ -6,20 +6,48 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -27,9 +55,6 @@ import androidx.navigation.NavController
 import br.com.mindbox.R
 import br.com.mindbox.components.AnimatedGradientBackground
 import br.com.mindbox.components.DrawerItem
-import br.com.mindbox.database.repository.EmailRepository
-import br.com.mindbox.database.repository.UserRepository
-import br.com.mindbox.service.AuthorizationService
 import br.com.mindbox.service.ChatBot
 
 data class ChatMessage(val text: String, val isUserMessage: Boolean)
@@ -42,15 +67,6 @@ fun ChatScreen(
     chatBot: ChatBot
 ) {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val coroutineScope = rememberCoroutineScope()
-    val context = LocalContext.current
-    val authorizationService = AuthorizationService(context)
-    val user = authorizationService.getLoggedUsers()[0];
-    val userRepository = UserRepository(context)
-    val emailRepository = EmailRepository(context)
-    val listRecipients: List<Long> = listOf<Long>(user.id)
-    val usersWithRecentEmails = remember { userRepository.findUsersWithRecentEmailsSent(user.id) }
-    val findEmailsSentToUsers = remember { emailRepository.findEmailsSentToUsers(listRecipients) }
     val startAnimation by remember { mutableStateOf(false) }
     val alphaAnim: State<Float> = animateFloatAsState(
         targetValue = if (startAnimation) 1f else 0f,
