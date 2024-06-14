df6ba6f1a03fe6cde9dacb6d8a6038a77a7338f0
daniel25778 <danielwilsonca94@gmail.com>
2024-06-14 00:19:56 -0300

    fix: adjust on start screen

--- app/src/main/java/br/com/mindbox/MainActivity.kt
+++ app/src/main/java/br/com/mindbox/MainActivity.kt
@@ -66,7 +66,7 @@ class MainActivity : ComponentActivity() {
 
                     AnimatedNavHost(
                         navController = navController,
-                        startDestination = "calendar",
+                        startDestination = "splash",
                         exitTransition = {
                             slideOutOfContainer(
                                 towards =
